1bc28caddc5f73370c9a8dedab0de5433d6cf6d1
mclarke <mclarkestudio@gmail.com>
2023-02-15 10:57:58 -0500

    add genRandomeTree util and render 1000 nodes

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,25 +1,16 @@
 import React from "react";
 import dynamic from "next/dynamic";
-import { GraphData } from "react-force-graph-3d";
 import SpriteText from "three-spritetext";
+import { genRandomTree } from "@/utils";
 
-/**
- * Import dynamically to avoid server calls to window, self, etc.
- * - https://nextjs.org/docs/advanced-features/dynamic-import#with-no-ssr
- */
 const ForceGraph3D = dynamic(() => import("react-force-graph-3d"), {
   ssr: false,
 });
 
-const data: GraphData = {
-  nodes: [{ id: "Me" }, { id: "You" }],
-  links: [{ source: "Me", target: "You" }],
-};
-
 export default function Home() {
   return (
     <ForceGraph3D
-      graphData={data}
+      graphData={genRandomTree(1000)}
       nodeThreeObject={(node) => {
         const sprite = new SpriteText(`${node.id}`);
         sprite.color = "white";
