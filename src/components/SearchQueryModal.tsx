9b83f8d36f7ef0ce8bc39a70c1a93543a4dc4f7c
mclarke <mclarkestudio@gmail.com>
2023-02-16 10:57:42 -0500

    add AddPromptButton mark up

--- src/components/SearchQueryModal.tsx
+++ src/components/SearchQueryModal.tsx
@@ -1,19 +1,56 @@
 import StartInput from "./StartInput";
-import { Card, Chip, Label, Row, SearchIcon } from "./StyledComponents";
+import {
+  Card,
+  Chip,
+  Label,
+  PlusIcon,
+  Row,
+  SearchIcon,
+} from "./StyledComponents";
+import reactStringReplace from "react-string-replace";
+import { ReactNode, ReactNodeArray, useState } from "react";
+
+const RenderNodeArray = ({ data }: { data: ReactNodeArray }) => {
+  console.log(data);
+  data.map((node: any) => {
+    const isMatch = typeof node === "object";
+    const rawText = isMatch && node.props.children;
+    console.log(isMatch && node.props.children);
+  });
+
+  return <>{data}</>;
+};
+
+const AddPromptButton = () => (
+  <div
+    className={
+      "ease-in duration-75 opacity-50 hover:opacity-100 animate cursor-pointer"
+    }
+  >
+    <Card>
+      <PlusIcon className="m-auto" />
+    </Card>
+  </div>
+);
 
 export default function SearchQueryModal({ onSubmit }: any) {
+  const [state, setState] = useState({
+    blocks: [{ label: undefined, value: "" }],
+    searchInput: "",
+  });
+
   return (
     <div className="inset-center center z-50" style={{ width: 600 }}>
       <Card>
         <Chip>
           <Row>
             <SearchIcon className="mr-2" />
-            <StartInput onSubmit={onSubmit} />
+            <StartInput onSubmit={onSubmit} value={state.searchInput} />
           </Row>
         </Chip>
       </Card>
       <Card>
-        <div className="mb-2 pr-4 py-1  w-fit rounded-full">
+        <div className="mb-3 pr-4 w-fit rounded-full">
           <Label>Add Label +</Label>
         </div>
         <Chip>
@@ -23,6 +60,7 @@ export default function SearchQueryModal({ onSubmit }: any) {
           />
         </Chip>
       </Card>
+      <AddPromptButton />
     </div>
   );
 }
--- src/components/StyledComponents.tsx
+++ src/components/StyledComponents.tsx
@@ -1,5 +1,8 @@
 export const Card = (props: any) => (
-  <div {...props} className="rounded-2xl bg-neutral-900 p-5 mb-6" />
+  <div
+    {...props}
+    className="px-4 py-4 mb-5 bg-neutral-900 rounded-3xl border border-slate-500/30"
+  />
 );
 
 export const Chip = (props: any) => (
@@ -39,6 +42,22 @@ export const SearchIcon = ({ className }: { className?: string }) => (
       stroke-width="2"
       stroke-linecap="round"
       stroke-linejoin="round"
+    />
+  </svg>
+);
+
+export const PlusIcon = ({ className }: { className?: string }) => (
+  <svg
+    width="16"
+    height="16"
+    viewBox="0 0 16 16"
+    fill="none"
+    xmlns="http://www.w3.org/2000/svg"
+    className={className}
+  >
+    <path
+      d="M7.06321 15.0358V0.649414H9.00639V15.0358H7.06321ZM0.835938 8.80851V6.87669H15.2337V8.80851H0.835938Z"
+      fill="white"
     />
   </svg>
 );
