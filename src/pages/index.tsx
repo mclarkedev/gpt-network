aa789e4e9e71565200d2320d862bdb26cd7fc1ac
mclarke <mclarkestudio@gmail.com>
2023-02-24 16:33:09 -0500

    add fullscreen loading indication after node click

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -4,12 +4,10 @@ import NavigationHeader from "@/components/NavigationHeader";
 
 export default function Graph() {
   return (
-    <div className="">
+    <div>
       <NavigationHeader />
       <InteractiveForceGraph />
-      {/* {/* <div className="absolute top-0 bottom-0 left-0 right-0 bg-black z-50 mix-blend-screen touch-none pointer-events-none" /> */}
       {/* <div className="fixed top-0 bottom-0 left-0 right-0 bg-stone-100 z-50 mix-blend-difference touch-none pointer-events-none" /> */}
-      {/* <div className="fixed top-0 bottom-0 left-0 right-0 bg-blue-100 z-50 mix-blend-multiply touch-none pointer-events-none" /> */}
     </div>
   );
 }
