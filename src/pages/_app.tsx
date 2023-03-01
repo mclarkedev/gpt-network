0941cd28aff6456ffb08bfb36d12c388c0d2a0d5
mclarke <mclarkestudio@gmail.com>
2023-03-01 13:06:51 -0500

    refactor DevStats

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -2,7 +2,7 @@ import "@/styles/globals.css";
 import type { AppProps } from "next/app";
 import { Inter, IBM_Plex_Sans } from "@next/font/google";
 import { RecoilRoot } from "recoil";
-import Script from "next/script";
+import DevStats from "@/components/Three/DevStats";
 
 const inter = Inter({ subsets: ["latin"] });
 const plex = IBM_Plex_Sans({ weight: "300", subsets: ["latin"] });
@@ -10,27 +10,7 @@ const plex = IBM_Plex_Sans({ weight: "300", subsets: ["latin"] });
 export default function App({ Component, pageProps }: AppProps) {
   return (
     <RecoilRoot>
-      {process.env.NODE_ENV === "development" && (
-        <Script id="show-banner" strategy="afterInteractive">
-          {(function () {
-            if (typeof document === "undefined") {
-              return;
-            }
-            var script = document.createElement("script");
-            script.onload = function () {
-              var stats = new Stats();
-              document.body.appendChild(stats.dom);
-              requestAnimationFrame(function loop() {
-                stats.update();
-                requestAnimationFrame(loop);
-              });
-            };
-            script.src = "//mrdoob.github.io/stats.js/build/stats.min.js";
-            document.head.appendChild(script);
-            return null;
-          })()}
-        </Script>
-      )}
+      <DevStats />
       <main className={inter.className}>
         <Component {...pageProps} />
         <div className={plex.className} />
