2679f5471c10024600a75fa248a0e75300bac073
mclarke <mclarkestudio@gmail.com>
2023-03-06 15:58:04 -0500

    add sticky node header

--- src/components/GraphListPanel.tsx
+++ src/components/GraphListPanel.tsx
@@ -9,7 +9,7 @@ export default function GraphDataPanel() {
 
   useEffect(() => {
     setMounted(true);
-  });
+  }, [setMounted]);
 
   const render = mounted && nodes.length;
   return render ? (
@@ -17,7 +17,7 @@ export default function GraphDataPanel() {
       className="fixed left-6 top-6 z-50 overflow-scroll rounded-xl"
       style={{ maxHeight: "calc(100vh - 3rem)" }}
     >
-      <div className="px-3 py-2 pt-3 bg-neutral-800 text-neutral-500 text-sm">
+      <div className="sticky top-0 px-3 py-2 pt-3 bg-neutral-800 text-neutral-500 text-sm">
         Nodes
       </div>
       {nodes.map((node) => {
@@ -27,20 +27,6 @@ export default function GraphDataPanel() {
             className="px-3 py-1 text-neutral-400 text-sm w-[200px] bg-neutral-800  hover:bg-neutral-700 hover:text-white"
           >
             {node.id}
-          </div>
-        );
-      })}
-      <div className="px-3 py-2 pt-3 bg-neutral-800 text-neutral-500 text-sm">
-        Links
-      </div>
-      {links.map((link) => {
-        const linkText = `${link.source} -> ${link.target} `;
-        return (
-          <div
-            key={linkText}
-            className="px-3 py-1 text-neutral-400 text-sm w-[200px] bg-neutral-800  hover:bg-neutral-700 hover:text-white"
-          >
-            {linkText}
           </div>
         );
       })}
