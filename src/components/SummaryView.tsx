8a67c98d894a199bb16edbce1e380e7a304c74af
mclarke <mclarkestudio@gmail.com>
2023-03-07 16:34:21 -0500

    clean unused SummaryView code

--- src/components/SummaryView.tsx
+++ src/components/SummaryView.tsx
@@ -1,105 +1,34 @@
-import {
-  Dispatch,
-  SetStateAction,
-  useCallback,
-  useEffect,
-  useRef,
-  useState,
-} from "react";
-import { useRecoilState, useRecoilValue, useSetRecoilState } from "recoil";
+import { useCallback } from "react";
+import { useRecoilState, useSetRecoilState } from "recoil";
 
-import {
-  contextMenuState,
-  focusedNodeIdState,
-  graphDataState,
-  summaryViewState,
-} from "@/state";
-import { NodeObject } from "react-force-graph-3d";
+import { focusedNodeIdState, summaryViewState } from "@/state";
 import PopUp from "./PopUp";
-import { LoadingIcon } from "./Icons";
-
-// type ContextMenuItem = {
-//   name: string;
-//   onClick: () => void;
-// };
 
 /**
- * Context Menu
+ * SummaryView
  */
 export default function SummaryView({
   resumeAnimation,
-}: //   handleGraphNodeClick,
-{
+}: {
   resumeAnimation: () => void;
-  handleGraphNodeClick: (nodeId?: string | number) => void;
 }) {
-  // UI state
   const [{ show, position, text }, setContextMenuState] = useRecoilState(summaryViewState); // prettier-ignore
   const setFocusedNodeId = useSetRecoilState(focusedNodeIdState);
-  //   const [activeItem, setActiveItem] = useState<number>(0); // where 0 is null
-
-  // Action state
-  //   const focusedNodeId = useRecoilValue(focusedNodeIdState);
-  //   const [graphData, setGraphData] = useRecoilState(graphDataState);
-  //   const [
-  //     { show: showSummary, position: summaryPosition },
-  //     setSummaryViewState,
-  //   ] = useRecoilState(summaryViewState);
 
   /**
-   * Reset to 0 when shown again
+   * closePopUp
    */
-  //   useEffect(() => {
-  //     setActiveItem(0);
-  //   }, [showSummaryView]);
-
-  /**
-   * CloseContextMenu
-   */
-  const closeContextMenu = useCallback(() => {
+  const closePopUp = useCallback(() => {
     resumeAnimation();
-    // setActiveItem(0);
     setContextMenuState((contextMenu) => ({ ...contextMenu, show: false }));
     setFocusedNodeId(undefined);
-  }, [resumeAnimation, setContextMenuState]);
-
-  /**
-   * Handle key roving
-   */
-  //   useEffect(() => {
-  //     function handleKeyDown(event: KeyboardEvent) {
-  //       if (event.key === "ArrowUp") {
-  //         activeItem > 1 && setActiveItem(activeItem - 1);
-  //       }
-  //       if (event.key === "ArrowDown") {
-  //         activeItem < maxItems && setActiveItem(activeItem + 1);
-  //       }
-  //       if (event.key === "Enter") {
-  //         // Enter is handled through native focus tracking
-  //       }
-  //       if (event.key === "Escape") {
-  //         closeContextMenu();
-  //       }
-  //     }
-
-  //     document.addEventListener("keydown", handleKeyDown);
-
-  //     return () => {
-  //       document.removeEventListener("keydown", handleKeyDown);
-  //     };
-  //   }, [
-  //     activeItem,
-  //     setActiveItem,
-  //     setContextMenuState,
-  //     maxItems,
-  //     closeContextMenu,
-  //   ]);
+  }, [resumeAnimation, setContextMenuState, setFocusedNodeId]);
 
   /**
    * Show
    */
   return show ? (
-    <PopUp position={position} onClickOutside={() => closeContextMenu()}>
+    <PopUp position={position} onClickOutside={() => closePopUp()}>
       <div className="px-1 py-1 w-[40ch]">
         {text ? <div className="text-sm p-2 text-white">{text}</div> : "..."}
       </div>
